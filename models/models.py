958332ee5911b3037be6c26f49fd28d301065db0
VitaliiHlaphun <v.glapshun@ukr.net>
2020-12-30 14:13:48 +0200

    actions inheritance

--- models/models.py
+++ models/models.py
@@ -4,21 +4,30 @@ from odoo import models, fields, api
 
 class ProjectTaskInherit(models.Model):
     _inherit = 'project.task'
-
-    complexity = fields.Char(string='Complexity')
-    skill_level = fields.Char(string='Skill level')
+    complexity = fields.Char()
+    skill_level = fields.Char()
     urgency = fields.Boolean()
+    experience = fields.Integer()
+
+class Student(models.Model):
+    _name = 'first_module.student'
+    _description = "student"
+
+    name = fields.Char()
+    birth_date = fields.Datetime(string="Date of birth")
+
+    module_ids = fields.One2many('first_module.first_module', 'student_id', string="Module")
 
 class first_module(models.Model):
     _name = 'first_module.first_module'
-    _description = 'first_module.first_module'
+    _description = 'first_module'
 
     name = fields.Char()
-    lastname = fields.Boolean()
     value = fields.Integer()
     value2 = fields.Float(compute="_value_pc", store=True)
     description = fields.Text()
-    birth_date = fields.Datetime(string="Date of birth")
+
+    student_id = fields.Many2one('first_module.student', string="Student")
 
     # @api.depends('value')
     # def _value_pc(self):
