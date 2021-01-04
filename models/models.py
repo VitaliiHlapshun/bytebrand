51834d3fe872d0b18589a20b7ad0b61d94189895
VitaliiHlaphun <v.glapshun@ukr.net>
2021-01-04 15:56:10 +0200

    default values, attrs, decorators, computed

--- models/models.py
+++ models/models.py
@@ -9,27 +9,43 @@ class ProjectTaskInherit(models.Model):
     urgency = fields.Boolean()
     experience = fields.Integer()
 
-class Student(models.Model):
-    _name = 'first_module.student'
-    _description = "student"
-
-    name = fields.Char()
-    birth_date = fields.Datetime(string="Date of birth")
-
-    module_ids = fields.One2many('first_module.first_module', 'student_id', string="Module")
-
 class first_module(models.Model):
     _name = 'first_module.first_module'
     _description = 'first_module'
 
+    def _dafault_description(self):
+        return "... Python Developer"
     name = fields.Char()
-    value = fields.Integer()
-    value2 = fields.Float(compute="_value_pc", store=True)
-    description = fields.Text()
+    total_score = fields.Integer(string="Total Score")
+    mark = fields.Float(compute="_value_pc", store=True)
+    status = fields.Char(compute="_status")
+    description = fields.Text(string="Description", default=_dafault_description)
+    is_registered = fields.Boolean('Registered', copy=False)
 
-    student_id = fields.Many2one('first_module.student', string="Student")
+    student_picture = fields.Binary(string='Picture')
 
-    # @api.depends('value')
-    # def _value_pc(self):
-    #     for record in self:
-    #         record.value2 = float(record.value) / 100
+    parents_id = fields.Many2one('first_module.parents', string="Parents",
+                                 required=False, default=lambda self: self.env.user)
+
+    class Parents(models.Model):
+        _name = 'first_module.parents'
+        _description = "parents"
+
+        name_of_mother = fields.Char()
+        name_of_father = fields.Char()
+        birth_date = fields.Date(string="Date of birth", default=fields.Date.today)
+
+        module_ids = fields.One2many('first_module.first_module', 'parents_id', string="Module")
+
+    @api.depends('total_score')
+    def _value_pc(self):
+        for record in self:
+            record.mark = float(record.total_score) / 100
+
+    @api.depends('mark')
+    def _status(self):
+        for record in self:
+            if record.mark >= 1.25:
+                record.status = "Passed"
+            else:
+                record.status = "Not passed"
